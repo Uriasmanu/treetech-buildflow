d1422b62a60b141d7aceb4c4bdeba9b99b5a18e5
Manoela Urias <manoela.urias@treetech.com.br>
2025-08-24 12:34:32 -0300

    Cria a primeira pagina

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,20 @@
-import Image from "next/image";
+import BuildeForm from "./components/BuildeForm";
+import Iniciar from "./components/Iniciar";
 
 export default function Home() {
   return (
-   <div></div>
+    <div className="flex w-full h-screen">
+      <section className="flex w-1/2 h-full bg-gray-100 justify-center items-center">
+        <h1 className="text-emerald-950 text-8xl font-bold text-left">
+          <span className="block">Builder</span>
+          <span className="block">Maps</span>
+        </h1>
+      </section>
 
+      <section className="flex flex-col w-1/2 h-full bg-green-700 p-6 gap-2 items-center gap-6 justify-center">
+        <h2 className="text-amber-50 font-bold text-4xl text-center">Como deseja Iniciar?</h2>
+        <Iniciar />
+      </section>
+    </div>
   );
 }

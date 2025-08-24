439a2eff463833d522c97a79abe6ff77853e7520
Manoela Urias <manoela.urias@treetech.com.br>
2025-08-24 14:34:13 -0300

    Reafatora para organizar o codigo

---
+++ src/app/components/Cabecalho/index.tsx
@@ -0,0 +1,22 @@
+import BotaoVoltar from "../BotaoVoltar";
+
+interface CabecalhoProps{
+  mostrarBotao?: boolean;
+}
+
+export default function Cabecalho({mostrarBotao = true}: CabecalhoProps) {
+    return (
+    <section className="relative flex w-1/2 h-full bg-gray-100 justify-center items-center">
+      {mostrarBotao && (
+        <div className="absolute top-4 left-4">
+          <BotaoVoltar />
+        </div>
+      )}
+        <h1 className="text-emerald-950 text-8xl font-bold text-left">
+          <span className="block">Builder</span>
+          <span className="block">Maps</span>
+        </h1>
+      </section>
+
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
 import BuildeForm from "./components/BuildeForm";
+import Cabecalho from "./components/Cabecalho";
 import Iniciar from "./components/Iniciar";
 
 export default function Home() {
   return (
     <div className="flex w-full h-screen">
-      <section className="flex w-1/2 h-full bg-gray-100 justify-center items-center">
-        <h1 className="text-emerald-950 text-8xl font-bold text-left">
-          <span className="block">Builder</span>
-          <span className="block">Maps</span>
-        </h1>
-      </section>
+      <Cabecalho mostrarBotao={false} />
 
       <section className="flex flex-col w-1/2 h-full bg-green-700 p-6 gap-2 items-center gap-6 justify-center">
         <h2 className="text-amber-50 font-bold text-4xl text-center">Como deseja Iniciar?</h2>
